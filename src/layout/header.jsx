19c72814e8116cf1edacfae5c52a50cbb1420aa4
yezzle <yezzle@codepku.com>
2020-01-06 22:56:15 +0800

    更新header组件

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-import { PageHeader } from 'antd'
-
+import { PageHeader, Menu, Dropdown, Button } from 'antd'
+import './header.less'
+import brand from './brand.png';
+const { Item } = Menu;
 export default class Header extends Component {
     constructor() {
         super()
@@ -9,20 +11,38 @@ export default class Header extends Component {
         }
     }
 
+    renderContent = () => {
+        switch (this.props.location.pathname) {
+            case '/': return <></>          //首页
+            case '/about': return <></>     //关于柏商
+            case '/contact': return <></>   //联系我们
+            case '/band': return <></>      //品牌运营
+            case '/custom': return <></>    //企业定制
+            case '/gift': return <></>      //礼盒守信
+        }
+    }
+
+    renderMenu = () => {
+        return <Menu mode="horizontal">
+            <Item>
+                <span className="header-menu-item">简</span>
+            </Item>
+            <Item>
+                <span className="header-menu-item">en</span>
+            </Item>
+        </Menu>
+    }
+
     render() {
+        const menu = this.renderMenu();
         return (
             <PageHeader
-                style={{
-                    position: "absolute",
-                    left: 0,
-                    top: 0,
-                    width: '100%',
-                    border: '1px solid rgb(235, 237, 240)',
-                }}
-                onBack={() => null}
-                title="Title"
-                subTitle="This is a subtitle"
-            />
+                className="layout-header"
+                extra={menu}
+                title={<img className="header-brand" src={brand}></img>}
+            >
+
+            </PageHeader>
         )
     }
 }
