c8164a4bb44bbfaa4719d172e77756fad817332b
吴博昊 <wbhoct@163.com>
2020-01-08 16:02:40 +0800

    modify: 基础样式修改

---
+++ src/components/textPlay.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Texty from 'rc-texty'
+
+class TextPlay extends Component {
+
+
+
+    render() {
+        return(
+            <Texty>
+                456456456456
+            </Texty>
+        )
+    }
+}
+
+export default TextPlay
+
