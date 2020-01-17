2cdc9639755d45061f8604d2d59476944249cb8b
zhangye <yezzle@codepku.com>
2020-01-17 16:20:14 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,14 @@ class App extends React.Component {
     // 适配手机屏幕;
     enquireScreen((b) => {
       this.setState({ isMobile: !!b });
+      if(b){
+        let w = window.innerWidth, h = window.innerHeight
+        let minWidth = 1300;
+        let scale = Math.min( w/minWidth, 1.0 )
+        document.children[0].style['font-size'] = `${80*Math.max(scale,1.0)}px`
+        document.body.setAttribute('style', `min-width: ${w/scale}px; transform: scale(${scale}); transform-origin: 0 0; height: ${h/scale}px`)
+        
+      }
     });
   }
 
